a3b1188af63e44779c116ea5d1f0dc1d68032f19
ocarrara <ocarrara@sorint.com>
2025-06-20 08:28:04 +0200

    test on push coolify

--- obj/Debug/net8.0/PortfolioApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/PortfolioApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95f25eda0210eee11c56ac62757192ed869d58bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30ebd321613757dd9aa24e9fbd0208dc16480da2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
